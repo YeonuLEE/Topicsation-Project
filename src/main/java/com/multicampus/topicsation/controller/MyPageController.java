f313ce5bf9af48d2a71072217c575132ea7957fa
Myeongjin <mangs2e@naver.com>
2023-04-19 16:53:04 +0900

    230419 튜티 마이페이지 - 히스토리 정보 ajax 작업

--- src/main/java/com/multicampus/topicsation/controller/MyPageController.java
+++ src/main/java/com/multicampus/topicsation/controller/MyPageController.java
@@ -1,5 +1,7 @@
 package com.multicampus.topicsation.controller;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,9 +51,30 @@ public class MyPageController {
 
         }
 
-        @GetMapping("/{user_id}/history.get")
-        public void historyPage(){
+        @GetMapping("/{user_id}/history/get")
+        public String historyPage(){
+            JSONObject jsonObject = new JSONObject();
+            JSONArray jsonArray = new JSONArray();
+            JSONObject obj1 = new JSONObject();
+            obj1.put("class_date", "2023-04-16 10:00AM");
+            obj1.put("tutor_name", "Jonny Dep");
+            obj1.put("memo", "20200416.txt");
+            jsonArray.add(obj1);
 
+            JSONObject obj2 = new JSONObject();
+            obj2.put("class_date", "2023-04-18 11:30AM");
+            obj2.put("tutor_name", "Angeli Remy");
+            obj2.put("memo", "20200418.txt");
+            jsonArray.add(obj2);
+
+            jsonObject.put("name","김명진");
+            jsonObject.put("user_id","3125");
+            jsonObject.put("history",jsonArray);
+
+            String jsonString = jsonObject.toString();
+            System.out.println(jsonString);
+
+            return jsonString;
         }
 
 
