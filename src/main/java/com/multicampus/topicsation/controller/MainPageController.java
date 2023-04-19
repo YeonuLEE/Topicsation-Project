a3c38913a5c952e31e9f43f5083909ae7e086881
AngryCatKR <thsehddud123@naver.com>
2023-04-19 14:56:26 +0900

    tutor 상세보기 : info와 schedule, Server와 통신해서 data 주고 받는 기능 개발(ver 1)

--- src/main/java/com/multicampus/topicsation/controller/MainPageController.java
+++ src/main/java/com/multicampus/topicsation/controller/MainPageController.java
@@ -1,10 +1,7 @@
 package com.multicampus.topicsation.controller;
 
-import org.json.simple.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/main")
@@ -42,11 +39,13 @@ public class MainPageController {
 
         }
 
-        @GetMapping("/tutors/{tutor_id}/get")
-        public String tutors(){
-            String jsonString = "{\"user_id\" : \"1234\",\"name\" : \"Tom hardy\",\"like\" : \"123\",\"nationality\" : \"europe\" ,\"interest1\" : \"fitness\",\"interest2\" : \"food\",\"introduce\" : \"안녕하세요~\",\"picture\" : \"image.jpg\"}";
+        @GetMapping("/tutors/{tutor_id}/getInfo")
+        public String tutors(@PathVariable("tutor_id") String tutorId,
+                             @RequestParam("calendarDate") String calendarDate) {
+            System.out.println(tutorId);
+            System.out.println(calendarDate);
+            String jsonString = "{\"schedule\" : [{\"class_id\" : \"5555\",\"class_date\" : \"2023-04-13\",\"class_time\" : \"0500PM\",\"tutee_id\" : \"null\",\"tutor_id\" : \"1234\"},{\"class_id\" : \"5556\",\"class_date\" : \"2023-04-13\",\"class_time\" : \"0530PM\",\"tutee_id\" : \"null\",\"tutor_id\" : \"1234\"}] ,\"tutor_info\":{\"user_id\" : \"1234\",\"name\" : \"Tomm hardy\",\"like\" : \"123\",\"nationality\" : \"europe\" ,\"interest1\" : \"fitness\",\"interest2\" : \"food\",\"introduce\" : \"안녕하세요~\",\"picture\" : \"data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg==\"}}";
             return jsonString;
         }
-
     }
 }
