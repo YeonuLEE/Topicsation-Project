42fe568aefbb95de5e75beff9f3dbea360479820
Yeonu <dusdn8060@naver.com>
2023-04-19 14:04:14 +0900

    main, main-search 튜터 Ajax 기능 추가 MainPageController 에 테스트 데이터 추가(삭제예정)

--- src/main/java/com/multicampus/topicsation/controller/MainPageController.java
+++ src/main/java/com/multicampus/topicsation/controller/MainPageController.java
@@ -32,14 +32,76 @@ public class MainPageController {
     @RequestMapping("/main")
     public class MainPageRestController{
 
-        @GetMapping(".get")
-        public void main(){
-
+        @GetMapping("/get")
+        public String main(){
+            String jsonString = "{\n" +
+                    "    \"tutor_list\" : [\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1001\",\n" +
+                    "            \"name\" : \"Yeonu LEE\",\n" +
+                    "            \"tutor_image\": \"1001.jpg\",\n" +
+                    "            \"like\" : \"320\",\n" +
+                    "            \"nationality\" : \"North America\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Politics\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1004\",\n" +
+                    "            \"name\" : \"Ahyeon LEE\",\n" +
+                    "            \"tutor_image\": \"1004.jpg\",\n" +
+                    "            \"like\" : \"230\",\n" +
+                    "            \"nationality\" : \"Europe\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Food\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1002\",\n" +
+                    "            \"name\" : \"Myeong Jin\",\n" +
+                    "            \"tutor_image\": \"1002.jpg\",\n" +
+                    "            \"like\" : \"200\",\n" +
+                    "            \"nationality\" : \"Asia\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Fitness\"\n" +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}";
+            return jsonString;
         }
 
         @GetMapping("/search-all.get")
-        public void searchAll(){
-
+        public String searchAll(){
+            String jsonString = "{\n" +
+                    "    \"tutor_list\" : [\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1001\",\n" +
+                    "            \"name\" : \"Yeonu LEE\",\n" +
+                    "            \"tutor_image\": \"1001.jpg\",\n" +
+                    "            \"like\" : \"320\",\n" +
+                    "            \"nationality\" : \"North America\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Politics\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1004\",\n" +
+                    "            \"name\" : \"Ahyeon LEE\",\n" +
+                    "            \"tutor_image\": \"1004.jpg\",\n" +
+                    "            \"like\" : \"230\",\n" +
+                    "            \"nationality\" : \"Europe\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Food\"\n" +
+                    "        },\n" +
+                    "        {\n" +
+                    "            \"user_id\" : \"1002\",\n" +
+                    "            \"name\" : \"Myeong Jin\",\n" +
+                    "            \"tutor_image\": \"1002.jpg\",\n" +
+                    "            \"like\" : \"200\",\n" +
+                    "            \"nationality\" : \"Asia\",\n" +
+                    "            \"interest1\" : \"IT\",\n" +
+                    "            \"interest2\" : \"Fitness\"\n" +
+                    "        }\n" +
+                    "    ]\n" +
+                    "}";
+            return jsonString;
         }
 
         @GetMapping("/tutors/{tutor_id}/get")
