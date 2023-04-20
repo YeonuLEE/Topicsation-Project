38f6fb82b5aa1e047913975bd9b939e49d1f61d2
Myeongjin <mangs2e@naver.com>
2023-04-20 12:02:09 +0900

    password-find js파일 분리

--- src/main/resources/static/js/validate-tutor.js
+++ src/main/resources/static/js/validate-tutor.js
@@ -146,31 +146,3 @@ $("#customFile").change(function () {
     var fileName = fileValue[fileValue.length - 1]; // 파일명
     $("#showFiles").text(fileName);
 });
-
-$(document).ready(function () {
-
-    $("#email").click(function () {
-
-        var user_id = "1234";
-        var email = "kk@gmail.com";
-
-        $.ajax({
-            type: "POST",
-            url: "/members/singin/find/post",
-            contentType: 'application/json',
-            data: JSON.stringify({
-                $user_id: user_id,
-                $email: email,
-                test: "test"
-            }),
-            success: function (data, status) {
-                console.log(data)
-            },
-            error: function (data, textStatus) {
-                alert("Error!")
-            },
-            complete: function (data, textStatus) {
-            },
-        });
-    });
-});
