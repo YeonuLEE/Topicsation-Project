458bac8eb7b3ba949e6ee0754799a592cba70ab9
Yeonu <dusdn8060@naver.com>
2023-04-20 11:46:51 +0900

    popup.js 평가 팝업 기능 추가

---
+++ src/main/resources/static/js/popup.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+   $('#like-btn').click(function () {
+      var pathURI = window.location.pathname
+      const regex = /\/(\d+)\//;
+      const match = pathURI.match(regex);
+      const number = match[1];
+      var ajaxURL = pathURI + ".do";
+
+      $.ajax({
+         url: ajaxURL,
+         type: "PUT",
+         contentType: 'application/json',
+         data: JSON.stringify({
+            $evaluate: 'like',
+            $lesson_id: number,
+            test: "test",
+         }),
+         success: function(result) {
+            // alert(result);
+            window.close();
+         },
+         error: function(xhr, status, error) {
+            console.error(error);
+         }
+      });
+   });
+   $('#dislike-btn').click(function () {
+      var pathURI = window.location.pathname
+      const regex = /\/(\d+)\//;
+      const match = pathURI.match(regex);
+      const number = match[1];
+      var ajaxURL = pathURI + ".do";
+
+      $.ajax({
+         url: ajaxURL,
+         type: "PUT",
+         contentType: 'application/json',
+         data: JSON.stringify({
+            $evaluate: 'dislike',
+            $lesson_id: number,
+            test: "test",
+         }),
+         success: function(result) {
+            // alert(result);
+            window.close();
+         },
+         error: function(xhr, status, error) {
+            console.error(error);
+         }
+      });
+   });
+});
