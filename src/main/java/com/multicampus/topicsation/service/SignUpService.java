7266a34d0d7d8668b95beedab2deb35bcd5b4239
Ahyuniii <bornthislee@gmail.com>
2023-04-26 00:51:46 +0900

    dto 추가, 비밀번호암호화 설정

--- src/main/java/com/multicampus/topicsation/service/SignUpService.java
+++ src/main/java/com/multicampus/topicsation/service/SignUpService.java
@@ -4,6 +4,7 @@ import com.multicampus.topicsation.dto.SignUpDTO;
 import com.multicampus.topicsation.repository.ISignUpDAO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 public class SignUpService implements ISignUpService {
     @Autowired
     private final ISignUpDAO dao;
+
+    private final PasswordEncoder passwordEncoder;
 
     // 아이디가 중복되는 경우 true룰 반환하며 그렇지 않은 경우 false를 반환
     @Override
@@ -24,6 +27,10 @@ public class SignUpService implements ISignUpService {
     // 회원으로 등록되었을 때 true를, 어떠한 이유때문에 등록되지 않은 경우 false를 반환
     @Override
     public int addTutee(SignUpDTO dto) {
+        //비밀번호 암호화
+        String encodedPassword = passwordEncoder.encode(dto.getPassword());
+        //인코딩한 비밀번호로 대체
+        dto.setPassword(encodedPassword);
         return dao.addTuteeDAO(dto);
     }
 
