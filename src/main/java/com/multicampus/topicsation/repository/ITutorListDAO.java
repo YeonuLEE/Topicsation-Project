8f6c3cd3bbc3df335e419513800a946de192a128
이동하 <tobetop2@naver.com>
2023-04-28 14:21:57 +0900

    main 페이지 : 추천

--- src/main/java/com/multicampus/topicsation/repository/ITutorListDAO.java
+++ src/main/java/com/multicampus/topicsation/repository/ITutorListDAO.java
@@ -1,8 +1,6 @@
 package com.multicampus.topicsation.repository;
 
-import com.multicampus.topicsation.dto.TutorReviewDTO;
-import com.multicampus.topicsation.dto.TutorScheduleDTO;
-import com.multicampus.topicsation.dto.TutorViewDTO;
+import com.multicampus.topicsation.dto.*;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -16,4 +14,10 @@ public interface ITutorListDAO {
     List<TutorScheduleDTO> tutorSchedule(Map <String,Object> paramMap);
     List<TutorReviewDTO> tutorReview(String tutorId);
     int classReserve(Map <String,Object> paramMap);
+
+    // 튜터 추천 메서드
+    MemberDTO tuteeInterest(String user_id);
+    List<RecommendDTO> recommendList(String interest1, String interest2);
+    List<RecommendDTO> spareList(String interest);
+    List<RecommendDTO> nonMembers();
 }
