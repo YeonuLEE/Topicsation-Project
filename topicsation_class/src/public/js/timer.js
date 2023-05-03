3c45d71715244d540d0a2dd0942fbe3ea5ae5747
AngryCatKR <thsehddud123@naver.com>
2023-05-04 00:55:42 +0900

    test용으로 만들어둔 로고 클릭시 메인으로 빠져나가게 하는 것 없앰

--- topicsation_class/src/public/js/timer.js
+++ topicsation_class/src/public/js/timer.js
@@ -3,31 +3,31 @@ $(document).ready(function () {
   var classId = window.location.pathname.split("/").pop(); // /class/456
   var apiUrl = pathURI.replace("{lesson_id}", classId);
 
-  $("#logo-main").click(function () {
-    var _width = "400";
-    var _height = "300";
-
-    // 팝업을 가운데 위치시키기 위해 아래와 같이 값 구하기
-    var _left = Math.ceil((window.screen.width - _width) / 2);
-    var _top = Math.ceil((window.screen.height - _height) / 2);
-
-    window.open(
-      apiUrl + "/evaluate",
-      "Evaluate",
-      "width=" +
-        _width +
-        ", height=" +
-        _height +
-        ", left=" +
-        _left +
-        ", top=" +
-        _top
-    );
-
-    popup.onload = function () {
-      window.opener.location.href = "http://localhost:8081/main";
-    };
-  });
+  // $("#logo-main").click(function () {
+  //   var _width = "400";
+  //   var _height = "300";
+  //
+  //   // 팝업을 가운데 위치시키기 위해 아래와 같이 값 구하기
+  //   var _left = Math.ceil((window.screen.width - _width) / 2);
+  //   var _top = Math.ceil((window.screen.height - _height) / 2);
+  //
+  //   window.open(
+  //     apiUrl + "/evaluate",
+  //     "Evaluate",
+  //     "width=" +
+  //       _width +
+  //       ", height=" +
+  //       _height +
+  //       ", left=" +
+  //       _left +
+  //       ", top=" +
+  //       _top
+  //   );
+  //
+  //   popup.onload = function () {
+  //     window.opener.location.href = "http://localhost:8081/main";
+  //   };
+  // });
 
   function remaindTime() {
     const curr = new Date();
