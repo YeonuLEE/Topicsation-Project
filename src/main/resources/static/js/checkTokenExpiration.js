f0e02be48d30274faf097f9de41c416ff026b88a
AngryCatKR <thsehddud123@naver.com>
2023-05-05 20:41:16 +0900

    5월 5일 refactoring 1. 토큰 검사 모듈 코드 간소화

--- src/main/resources/static/js/checkTokenExpiration.js
+++ src/main/resources/static/js/checkTokenExpiration.js
@@ -31,32 +31,26 @@ export function setupHeader(token) {
 }
 
 function checkTokenExp(accessToken) {
-    var base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
-    var payload = atob(base64Payload, 'base64');
-    var result = JSON.parse(payload.toString());
+    let base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+    let payload = atob(base64Payload, 'base64');
+    let result = JSON.parse(payload.toString());
 
     const expirationTime = result.exp * 1000 // exp는 초 단위이므로 밀리초 단위로 변환
-    if (expirationTime < Date.now()) {
-        // AccessToken이 만료된 경우
-        return true
-    } else {
-        // AccessToken이 유효한 경우
-        return false
-    }
+    return expirationTime < Date.now();
 }
 
 export function getId(accessToken) {
-    var base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
-    var payload = atob(base64Payload, 'base64');
-    var result = JSON.parse(payload.toString())
+    let base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+    let payload = atob(base64Payload, 'base64');
+    let result = JSON.parse(payload.toString())
 
     return result.sub;
 }
 
 export function getRoles(accessToken) {
-    var base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
-    var payload = atob(base64Payload, 'base64');
-    var result = JSON.parse(payload.toString())
+    let base64Payload = accessToken.split('.')[1]; //value 0 -> header, 1 -> payload, 2 -> VERIFY SIGNATURE
+    let payload = atob(base64Payload, 'base64');
+    let result = JSON.parse(payload.toString())
 
     return result.roles;
 }
