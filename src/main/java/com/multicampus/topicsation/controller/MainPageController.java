9f24b329854b7d067a84f155f5a07dfad34e6203
이동하 <tobetop2@naver.com>
2023-05-06 14:05:43 +0900

    SearchAll : 리펙토링

--- src/main/java/com/multicampus/topicsation/controller/MainPageController.java
+++ src/main/java/com/multicampus/topicsation/controller/MainPageController.java
@@ -1,20 +1,13 @@
 package com.multicampus.topicsation.controller;
 
-import com.multicampus.topicsation.dto.SearchDTO;
-import com.multicampus.topicsation.dto.pageDTO.PageRequestDTO;
-import com.multicampus.topicsation.dto.pageDTO.PageResponseDTO;
 import com.multicampus.topicsation.service.ITutorListService;
 import com.multicampus.topicsation.service.SearchService;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
 @Controller
@@ -54,45 +47,7 @@ public class MainPageController {
 
         @GetMapping("/search-all/get")
         public ResponseEntity<Map<String, Object>> searchPage(@RequestParam Map<String, String> requestParams) {
-            int page = Integer.parseInt(requestParams.getOrDefault("page", "1"));
-            int size = Integer.parseInt(requestParams.getOrDefault("size", "6"));
-            String name = requestParams.get("name");
-            String interest = requestParams.get("interest");
-            String date = requestParams.get("date");
-
-            if(date != null){
-                SimpleDateFormat inputFormat = new SimpleDateFormat("MM/dd/yyyy");
-                SimpleDateFormat outputFormat = new SimpleDateFormat("yyyy-MM-dd");
-
-                try {
-                    Date dateFormat = inputFormat.parse(date);
-                    date = outputFormat.format(dateFormat);
-                } catch (ParseException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            PageRequestDTO pageRequestDTO = PageRequestDTO.builder()
-                    .name(name)
-                    .interest(interest)
-                    .date(date)
-                    .page(page)
-                    .size(size)
-                    .build();
-
-            PageResponseDTO<SearchDTO> pageResponseDTO = searchService.searchList(pageRequestDTO);
-
-            Map <String, Object> resultMap = new HashMap<>();
-            resultMap.put("all_list",pageResponseDTO.getSearchDTOList());
-            resultMap.put("page",pageResponseDTO.getPage());
-            resultMap.put("size",pageResponseDTO.getSize());
-            resultMap.put("total",pageResponseDTO.getTotal());
-            resultMap.put("start",pageResponseDTO.getStart());
-            resultMap.put("end",pageResponseDTO.getEnd());
-            resultMap.put("prev",pageResponseDTO.isPrev());
-            resultMap.put("next",pageResponseDTO.isNext());
-
-           return new ResponseEntity<Map<String, Object>> (resultMap, HttpStatus.OK);
+            return searchService.searchList(requestParams);
         }
 
         @GetMapping("/tutors/{tutor_id}/getInfo")
