a66906958009e6653407cdf3b0ad7d553c524694
AngryCatKR <thsehddud123@naver.com>
2023-05-07 19:02:17 +0900

    5월 7일 refactoring 1. 쓰지 않는 메서드 정리

--- src/main/java/com/multicampus/topicsation/repository/IMemberDAO.java
+++ src/main/java/com/multicampus/topicsation/repository/IMemberDAO.java
@@ -21,15 +21,13 @@ public interface IMemberDAO {
     int modifyTutor(MyPageDTO myPageDTO);
     int modifyTutor2(MyPageDTO myPageDTO);
     
-    MypageScheduleDTO tutorProfile(String userId);
-    String checkProfileImg(String userId);
+    MyPageScheduleDTO tutorProfile(String userId);
     void changeProfileImg(@Param("userId") String userId, @Param("fileName") String fileName);
 
 
     List<ClassDTO> tutorSchedule(Map<String, Object> paramMap);
-    int findUser(Map<String, Object> paramMap);
     void scheduleDelete(Map<String, Object> scheduleMap);
-    int scheduleUpdate(Map<String, Object> scheduleMap);
+    int scheduleUpdate(Map<String, String> scheduleMap);
 
     List<ClassDTO> schedule(String userId);
     void deleteClass(String userId);
@@ -40,7 +38,7 @@ public interface IMemberDAO {
 
     MyPageDTO viewTutee(String userId);
     int modifyTutee(MyPageDTO myPageDTO);
-    MypageScheduleDTO tuteeProfile(String userId);
+    MyPageScheduleDTO tuteeProfile(String userId);
     List<ClassDTO> scheduleTutee(String userId);
     List<ClassDTO> historyTutee(String userId);
     void deleteTutee(String userId);
