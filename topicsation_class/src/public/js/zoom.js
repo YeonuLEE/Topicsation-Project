13bd2e9b8df4c394e91727ba79e902ec34501240
AngryCatKR <thsehddud123@naver.com>
2023-05-09 22:21:58 +0900

    iceServer url change

--- topicsation_class/src/public/js/zoom.js
+++ topicsation_class/src/public/js/zoom.js
@@ -163,11 +163,7 @@ $(document).ready(function () {
       iceServers: [
         {
           urls: [
-            "stun:stun.l.google.com:19302",
-            "stun:stun1.l.google.com:19302",
-            "stun:stun2.l.google.com:19302",
-            "stun:stun3.l.google.com:19302",
-            "stun:stun4.l.google.com:19302",
+            "http://27.96.131.49:3000"
           ],
         },
       ],
