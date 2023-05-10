4bea47bd0919e39af9787db33df61e22efc7c014
AngryCatKR <thsehddud123@naver.com>
2023-05-11 00:05:36 +0900

    CORS추가 및 TURN 서버 재설정

--- topicsation_class/src/server.js
+++ topicsation_class/src/server.js
@@ -3,11 +3,15 @@ import https from "https";
 import { Server } from "socket.io";
 import { instrument } from "@socket.io/admin-ui";
 import { filtering, AhoCorasick, wordsToCensor} from './censoredWords.js';
+const crypto = require('crypto');
+const cors = require('cors');
+
+require('dotenv').config();
 
 const app = require("./app");
 const fs = require('fs');
 
-// 인증서 파일 경로
+//인증서 파일 경로
 const privateKey = fs.readFileSync('/etc/letsencrypt/live/www.topicsation.online/privkey.pem', 'utf8');
 const certificate = fs.readFileSync('/etc/letsencrypt/live/www.topicsation.online/cert.pem', 'utf8');
 const ca = fs.readFileSync('/etc/letsencrypt/live/www.topicsation.online/chain.pem', 'utf8');
@@ -17,6 +21,35 @@ const credentials = {
   cert: certificate,
   ca: ca
 };
+
+// TURN server 아이디, 비밀번호 전달
+app.get('/getTurnCredentials', (req, res) => {
+  let staticAuthSecret = process.env.STATIC_AUTH_SECRET; // .env 파일의 STATIC_AUTH_SECRET 값
+  let username = Math.floor(Date.now() / 1000) + 3600; // 유닉스 타임스탬프 + 유효시간(여기서는 1시간)
+
+  let password = crypto.createHmac('sha1', staticAuthSecret)
+      .update(username.toString())
+      .digest('base64');
+
+  res.json({username, password});
+});
+
+let username = Math.floor(Date.now() / 1000) + 3600;
+let password = crypto.createHmac('sha1', "wrc0516!@")
+    .update(username.toString())
+    .digest('base64');
+console.log("username ", username)
+console.log("password ", password)
+
+// CORS 설정
+var corsOptions = {
+  origin: 'https://www.topicsation.online',
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+  allowedHeaders: "*"
+};
+
+app.use(cors(corsOptions));
 
 // 금지어 필터링 설정
 const ac  = new AhoCorasick();
